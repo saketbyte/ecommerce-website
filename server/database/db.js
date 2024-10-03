54e53d001cc37ad7c362e917dcaf2f17510f9ad0
saketbyte <samriddhsingh00@gmail.com>
2024-10-03 15:38:54 +0530

    converted url to string

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const Connection = async (url) => {
 	// console.log(url);
 	try {
-		await mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+		await mongoose.connect(url.toString(), { useUnifiedTopology: true, useNewUrlParser: true });
 		console.log("Database Connected Successfully !");
 	} catch (error) {
 		console.log("Error while connecting", error.message);
